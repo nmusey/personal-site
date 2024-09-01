cb1dc4749d7c8bc2b73b2d0b65c6f699326b2699
nmusey <nmusey@gmail.com>
2024-09-01 11:33:37 -0700

    Add RSS link to blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -6,6 +6,7 @@ export default async function BlogIndex() {
         <div className="flex flex-col justify-start">
             <h1 className="text-center">Blog Posts</h1>
             <div className="flex flex-col align-middle">
+                <p><a href="/rss.xml" rel="noreferrer" target="_blank">Subscribe to the RSS feed here.</a></p>
                 {
                     posts.map((post: Post) => (
                         <a href={`/blog/${post.slug}`} key={post.slug} className="text-center my-8">
